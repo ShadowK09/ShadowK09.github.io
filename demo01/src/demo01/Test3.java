63fba951fa2c7fb11a79318b29e51226c5a783a9
莫愁莫回首 <@172.23.48.1>
2018-04-12 13:52:56 +0800

    1.0

---
+++ demo01/src/demo01/Test3.java
@@ -0,0 +1,29 @@
+package demo01;
+/**
+* @author 莫愁莫回首
+* @创建时间：2018年3月15日 上午10:57:47
+*/
+public class Test3 {
+
+	public static void test() {
+		System.out.println("┏┓　　　┏┓");
+		System.out.println("┏┛┻━━━┛┻┓");
+		System.out.println("┃　　　　　   ┃ ");
+		System.out.println("┃　　 ━　　　┃");
+		System.out.println("┃　┳┛　┗┳　┃");
+		System.out.println("┃　　　　　   ┃");
+		System.out.println("┃　　 ┻　　　┃");
+		System.out.println("┃　　　　　   ┃");
+		System.out.println("┗━┓　　　┏━┛");
+		System.out.println("  ┃　　　┃");
+		System.out.println("  ┃　　　┃");
+		System.out.println("  ┃　　　┗━━━┓");
+		System.out.println("  ┃　　　　　　　┣┓");
+		System.out.println("  ┃　　　　　　　┏┛");
+		System.out.println("  ┗┓┓┏━┳┓┏┛");
+	}
+	
+	public static void main(String[] args) {
+		test();
+	}
+}
